4684a917b8946997d15b70f1424776d64578ebce
Deep Govani <deepgovani97@gmail.com>
2023-07-06 11:18:22 +0530

    add env

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
     <router-view />
+    <footer>Mode: {{env}} </footer>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      env: process.env.ENV
+    }
+  }
+}
+</script>
 
 <style></style>
